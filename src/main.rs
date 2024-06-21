0027130e4e4e0af35811749a1dcf966e01bd3a2f
Yehor Dykhov <hyoustone@yahoo.com>
2024-06-21 20:58:01 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        if input.trim() == "exit 0" {
-            break;
-        }
+        let trimmed_input = input.trim();
 
-        println!("{}: command not found", input.trim());
+        if trimmed_input == "exit 0" {
+            break;
+        } else if trimmed_input.contains("echo") {
+            let echo_text: Vec<&str> = input.split("echo").collect();
+            println!("{}", echo_text[1].trim());
+        } else {
+            println!("{}: command not found", trimmed_input);
+        }
     }
 }
