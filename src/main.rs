e28a765918dda85b65650111a1605e01e1140a69
Yehor Dykhov <hyoustone@yahoo.com>
2024-06-23 12:31:53 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -102,8 +102,7 @@ fn main() {
                 let found_path = contains_executable_file_by_path(command_text, &paths);
 
                 if let Some(_) = found_path {
-                    let _ = ProcessCommand::new(command_text).arg(arg_text).output();
-                    break;
+                    let _ = ProcessCommand::new(command_text).arg(arg_text).spawn().unwrap();
                 } else {
                     println!("{}: command not found", trimmed_input);
                 }
